4aee10a268b17bc33cb82985692105e939910d9b
MrDoobPG <46342172+MrDoobPG@users.noreply.github.com>
2019-10-11 18:09:05 +0200

    Update remove.sh

--- apps/templates/rutorrent/tasks/remove.sh
+++ apps/templates/rutorrent/tasks/remove.sh
@@ -1,31 +1,28 @@
 #/bin/bash
 ##remove troublemaker plugins
 
-1=docker exec rutorrent
 dir=app/rutorrent/plugins
 
-$1 apk update -y
-$1 apk upgrade
+apk update -y
+apk upgrade
 
-$1 apk install git -y
+apk install git -y
 
-$1 rm -rf $dir/_cloudflare/
-$1 rm -rf $dir/ipad/
-$1 rm -rf $dir/rssurlrewrite/
-$1 rm -rf $dir/extsearch/
-$1 rm -rf $dir/spectrogram/
-$1 rm -rf $dir/retrackers/
-$1 rm -rf $dir/geoip/
-$1 rm -rf $dir/lookat/
-$1 rm -rf $dir/cookies/
-$1 rm -rf $dir/loginmgr/
-$1 rm -rf $dir/screenshots/
-$1 rm -rf $dir/rutracker_check/
-$1 rm -rf $dir/mobile/
+rm -rf $dir/_cloudflare/
+rm -rf $dir/ipad/
+rm -rf $dir/rssurlrewrite/
+rm -rf $dir/extsearch/
+rm -rf $dir/spectrogram/
+rm -rf $dir/retrackers/
+rm -rf $dir/geoip/
+rm -rf $dir/lookat/
+rm -rf $dir/cookies/
+rm -rf $dir/loginmgr/
+rm -rf $dir/screenshots/
+rm -rf $dir/rutracker_check/
+rm -rf $dir/mobile/
 
 ##clone mobile new
-$1 git clone https://github.com/xombiemp/rutorrentMobile.git $dir/mobile
-
-echo "done"
+git clone https://github.com/xombiemp/rutorrentMobile.git $dir/mobile
 
 exit 0
